83f45a014b6bdbccdd3ee226c982cfb392e0d087
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-07 17:30:05 -0500

    Reorganize imports and code comments between main.dart and home_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebasedemo/firebase_options.dart';
 
 void main() async {
@@ -45,42 +44,3 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
     );
   }
 }
-
-/*
-Code Snippet Example: Firestore Query for Search
-Stream<QuerySnapshot> _searchProducts(String query) {
-  return FirebaseFirestore.instance
-      .collection('products')
-      .where('name', isGreaterThanOrEqualTo: query)
-      .where('name', isLessThanOrEqualTo: query + '\uf8ff')
-      .snapshots();
-}
-Code Snippet Example: Firestore Query for Price Filter
-Stream<QuerySnapshot> _filterProductsByPrice(double minPrice, double maxPrice) {
-  return FirebaseFirestore.instance
-      .collection('products')
-      .where('price', isGreaterThanOrEqualTo: minPrice)
-      .where('price', isLessThanOrEqualTo: maxPrice)
-      .snapshots();
-}
-*/
-
-/*
-// In Home Screen - Floating Action Button
-onPressed: () {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => AddEditItemScreen()),
-  );
-},
-
-// In Home Screen List Item - Edit Navigation
-onTap: () {
-  Navigator.push(
-    context,
-    MaterialPageRoute(
-      builder: (context) => AddEditItemScreen(item: item),
-    ),
-  );
-},
-*/
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebasedemo/firebase_options.dart';
 import 'package:firebasedemo/models/firestore_service.dart';
 import 'package:firebasedemo/models/item.dart';
 
@@ -182,6 +180,24 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
         },
         child: Icon(Icons.add),
       ),
+
+      // In Home Screen - Floating Action Button
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => AddEditItemScreen()),
+        );
+      },
+
+      // In Home Screen List Item - Edit Navigation
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AddEditItemScreen(item: item),
+          ),
+        );
+      },
       */
     );
   }
