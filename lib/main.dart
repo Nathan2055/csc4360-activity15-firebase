9b88fe106de9a6a81ec4ca15831931fe9dfc2a50
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-07 17:41:53 -0500

    Fix naming conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebasedemo/firebase_options.dart';
+import 'package:firebasedemo/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(const FirebaseDemoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// TODO: 1. Initialize Firestore & Create a Stream for items
+// TODO: 2. Build a ListView using a StreamBuilder to display items
+// TODO: 3. Implement Navigation to an "Add Item" screen
+// TODO: 4. Implement one of the Delete methods (swipe or in-edit)
+
+class FirebaseDemoApp extends StatelessWidget {
+  const FirebaseDemoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,29 +24,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Inventory Management App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: InventoryHomePage(),
-    );
-  }
-}
-
-class InventoryHomePage extends StatefulWidget {
-  const InventoryHomePage({super.key});
-
-  @override
-  State<InventoryHomePage> createState() => _InventoryHomePageState();
-}
-
-class _InventoryHomePageState extends State<InventoryHomePage> {
-  // TODO: 1. Initialize Firestore & Create a Stream for items
-  // TODO: 2. Build a ListView using a StreamBuilder to display items
-  // TODO: 3. Implement Navigation to an "Add Item" screen
-  // TODO: 4. Implement one of the Delete methods (swipe or in-edit)
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('CRUD operations')),
-      body: InventoryHomePage(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('CRUD operations')),
+        body: InventoryHomePage(),
+      ),
     );
   }
 }
