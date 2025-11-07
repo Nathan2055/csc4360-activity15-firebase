24950670bad4ef7c900e4dc7ac65b2aee95feaa3
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-06 21:30:20 -0500

    Refactor, combine, and rename the starting code files

--- lib/models/firestore_service.dart
+++ lib/models/firestore_service.dart
@@ -1,21 +1,21 @@
 class FirestoreService {
   // TODO: Create collection reference for 'items'
-  
+
   // TODO: Implement addItem method
   Future<void> addItem(Item item) async {
     // TODO: Convert item to map and add to collection
   }
-  
+
   // TODO: Implement getItemsStream method
   Stream<List<Item>> getItemsStream() {
     // TODO: Return stream of items from Firestore
   }
-  
+
   // TODO: Implement updateItem method
   Future<void> updateItem(Item item) async {
     // TODO: Update specific document by ID
   }
-  
+
   // TODO: Implement deleteItem method
   Future<void> deleteItem(String itemId) async {
     // TODO: Delete document by ID
--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,15 +1,15 @@
 class Item {
   // TODO: Declare fields (id, name, quantity, price, category, createdAt)
-  
+
   // TODO: Create constructor with named parameters
-  
+
   // TODO: Implement toMap() for Firestore
   Map<String, dynamic> toMap() {
     return {
       // TODO: Convert all fields to map
     };
   }
-  
+
   // TODO: Implement fromMap() factory constructor
   factory Item.fromMap(String id, Map<String, dynamic> map) {
     return Item(
