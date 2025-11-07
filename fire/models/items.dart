419052a5607f72f00b6a666225786d19acb84e14
Alyvvn <muhammadalyan27@gmail.com>
2025-11-07 17:51:29 -0500

    finish model class

---
+++ fire/models/items.dart
@@ -0,0 +1,52 @@
+Class item {
+
+    final String id;
+    final String name;
+    final int quantity;
+    final double price;
+    final String category;
+    final Datetime createdAt;
+    
+
+
+    Item({
+        required this.name,
+        required this.quantity,
+        required this.price,
+        required this.category,
+        required this.createdAt,
+        
+    });
+
+}
+
+
+//convert item to a firestore-friendly map data
+
+Map<String, dynamic> toMap() {
+    return {
+        'id': id,
+        'name': name,
+        'quantity': quantity,
+        'price': price,
+        'category': category,
+        'createdAt': createdAt,
+        
+    };
+
+    //create an item from a firestore document snapshot
+    factory Item.fromSnapshot(DocumentSnapshot snapshot) {
+        final data = snapshot.data() as Map<String, dynamic>;
+        return Item(
+            id: snapshot.id,
+            name: data['name'],
+            quantity: data['quantity'],
+            price: data['price'],
+            category: data['category'],
+            createdAt: data['createdAt'].toDate(),
+        );
+    }
+}
+
+
+
