2c77b8d573877008f91423cb5e3a8deca8e93a93
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-07 19:58:58 -0500

    Rewrite main.dart for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebasedemo/firebase_options.dart';
-import 'package:firebasedemo/screens/home_screen.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebasedemo/firebase_options.dart';
+//import 'package:firebasedemo/screens/home_screen.dart';
+import 'package:firebasedemo/screens/login_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const FirebaseDemoApp());
 }
 
@@ -19,7 +20,7 @@ class FirebaseDemoApp extends StatelessWidget {
       title: 'Firebase Inventory Management',
       home: Scaffold(
         appBar: AppBar(title: const Text('Firebase Inventory Management')),
-        body: InventoryHomePage(),
+        body: LoginScreen(),
       ),
     );
   }
