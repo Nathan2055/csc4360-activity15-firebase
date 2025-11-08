49c8704fa54657dba369f3c0a02ca5e9d8b09772
Nathan Larsen <nlarsen@nathan2055.com>
2025-11-07 19:59:13 -0500

    Continue working on login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,9 +8,126 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  // Hardcoded logins
+  final String adminUsername = 'admin';
+  final String adminPassword = 'admin';
+  final String viewerUsername = 'viewer';
+  final String viewerPassword = 'viewer';
+
+  // Text field controllers
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  // Track password visibility
+  bool _isPasswordVisible = false;
+
+  void _submitForm() {
+    /*
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      // Simulate API call
+      Future.delayed(const Duration(seconds: 2), () {
+        if (!mounted) return; // Check if the widget is still in the tree
+        setState(() {
+          _isLoading = false;
+        });
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SuccessScreen(
+              userName: _nameController.text,
+              selectedIcon: _selectedIcon,
+              finalProfileCompletion: _progressValue,
+              finalPasswordStrength: _passwordStrengthValue,
+            ),
+          ),
+        );
+      });
+    }
+    */
+  }
+
+  Widget usernameField() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 24),
+      child: TextFormField(
+        controller: _usernameController,
+        decoration: InputDecoration(
+          labelText: 'Username',
+          prefixIcon: const Icon(Icons.person),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        ),
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 24),
+      child: TextFormField(
+        controller: _passwordController,
+        obscureText: !_isPasswordVisible,
+        decoration: InputDecoration(
+          labelText: 'Password',
+          prefixIcon: const Icon(Icons.lock),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          suffixIcon: IconButton(
+            icon: Icon(
+              _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+            ),
+            onPressed: () {
+              setState(() {
+                _isPasswordVisible = !_isPasswordVisible;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      onPressed: _submitForm,
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('Login')],
+      ),
+    );
+  }
+
+  Widget buildForm() {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [usernameField(), passwordField(), submitButton()],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    //throw UnimplementedError();
+
+    var form = buildForm();
+
+    return form;
   }
 }
